763d9c8f12f4dc1b21be1667ef40c0af04f709d5
Nacxht <zombiecrafter069@gmail.com>
2024-04-10 22:05:45 +0700

    created an reusable error handler (for api response)

---
+++ src/api/utils/err-responser.ts
@@ -0,0 +1,79 @@
+import { ErrorResponseSchema } from "../../../types/ErrorResponseSchema.js";
+import { logger } from "./winston.js";
+
+export const errorResponser = async (error: any) => {
+	// JSON schema
+	let errorResponseSchema: ErrorResponseSchema = {
+		status: null,
+		statusCode: null,
+		error: { name: "", path: "", message: "" },
+	};
+
+	// Error checking
+	if (error.name === "ValidationError" && error.isJoi && error.details[0].path[0] === "username") {
+		switch (error.details[0].type) {
+			default:
+				return (errorResponseSchema = {
+					status: false,
+					statusCode: 422,
+					error: { name: `Joi${error.name}`, path: "username", message: error.details[0].message },
+				});
+		}
+	} else if (error.name === "MongoServerError") {
+		switch (error.code) {
+			case 11000:
+				const path = Object.keys(error.keyValue)[0];
+				return (errorResponseSchema = {
+					status: false,
+					statusCode: 422,
+					error: { name: error.name, path, message: "Duplicate key" },
+				});
+
+			default:
+				logger.error(`${error.name} - ${error.message} - ${error.code}`);
+				return (errorResponseSchema = {
+					status: false,
+					statusCode: 422,
+					error: { name: error.name, path: "", message: error.message },
+				});
+		}
+	} else {
+		switch (error.name) {
+			case "User not found":
+				return (errorResponseSchema = {
+					status: false,
+					statusCode: 404,
+					error: { name: "CredentialError", path: "username", message: error.name },
+				});
+
+			case "Wrong password":
+				return (errorResponseSchema = {
+					status: false,
+					statusCode: 400,
+					error: { name: "CredentialError", path: "password", message: "Your password is incorrect" },
+				});
+
+			case "EmptyAuthToken":
+				return (errorResponseSchema = {
+					status: false,
+					statusCode: 401,
+					error: { name: "EmptyAuthToken", path: "", message: "Unauthorized" },
+				});
+
+			case "TokenExpiredError":
+				return (errorResponseSchema = {
+					status: false,
+					statusCode: 401,
+					error: { name: error.name, path: "", message: "Unauthorized" },
+				});
+
+			default:
+				logger.error(`${error.name} - ${error.message}`);
+				return (errorResponseSchema = {
+					status: false,
+					statusCode: 500,
+					error: { name: error.name, path: "", message: "Internal server error" },
+				});
+		}
+	}
+};
