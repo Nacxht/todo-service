fc9ae3e0154c3d0c4b52b5a7afcef099019fc77f
Nacxht <zombiecrafter069@gmail.com>
2024-04-10 22:04:42 +0700

    deleted unused import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response, NextFunction } from "express";
+import express, { Express } from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import { mongoInit } from "./config/mongo-conn.js";
