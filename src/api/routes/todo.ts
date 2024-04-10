8d07613945d3ba3dfa7e4d5dc2ef7c686cda31a1
Nacxht <zombiecrafter069@gmail.com>
2024-04-10 22:07:33 +0700

    added route for update todo

--- src/api/routes/todo.ts
+++ src/api/routes/todo.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { jwtMiddleware } from "../middlewares/jwt-middleware.js";
-import { todoCreateController, todoDeleteController, todoIndexController } from "../controller/todo-controllers.js";
+import { todoCreateController, todoDeleteController, todoIndexController, todoUpdateController } from "../controller/todo-controllers.js";
 
 export const todoRouter: Router = Router();
 
+// JWT Middleware
 todoRouter.use(jwtMiddleware);
 
 todoRouter.get("/", todoIndexController);
 todoRouter.post("/create", todoCreateController);
 todoRouter.delete("/delete", todoDeleteController);
+todoRouter.patch("/update", todoUpdateController);
