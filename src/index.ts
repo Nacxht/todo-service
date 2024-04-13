b39b097d33a91abe3d457f08210fd5c6627962e5
Nacxht <zombiecrafter069@gmail.com>
2024-04-13 22:42:57 +0700

    change cors just for development/local environtment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import cors from "cors";
+// import cors from "cors";
 import bodyParser from "body-parser";
 import { mongoInit } from "./config/mongo-conn.js";
 import { routeInit } from "./api/routes/init.js";
@@ -8,7 +8,7 @@ import { logger } from "./api/utils/winston.js";
 
 const app: Express = express();
 
-app.use(cors());
+// if (["development", "local"].includes(config.server.env)) app.use(cors());
 app.use(bodyParser.json());
 
 await mongoInit();
