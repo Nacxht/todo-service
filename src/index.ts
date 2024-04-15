c2da6d73d72d9f8098826979949a82dc5467a07c
Nacxht <zombiecrafter069@gmail.com>
2024-04-15 23:37:18 +0700

    conditional server run

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,33 @@ import { logger } from "./api/utils/winston.js";
 
 const app: Express = express();
 
-if (config.server.env === "production") {
-	app.use(
-		cors({
-			origin: ["https://zky-todo.vercel.app", "http://localhost:3000"],
-		})
-	);
-} else {
-	app.use(cors());
+try {
+	if (config.server.env === "production") {
+		app.use(
+			cors({
+				origin: ["https://zky-todo.vercel.app", "http://localhost:3000"],
+			})
+		);
+	} else {
+		app.use(cors());
+	}
+
+	app.use(bodyParser.json());
+
+	await mongoInit();
+	routeInit(app);
+
+	if (config.server.env === "production") {
+		app.listen(config.server.port, config.server.url, () => {
+			logger.info(`Running on ENV = '${config.server.env}'`);
+			logger.info(`Server is running at "${config.server.url}"`);
+		});
+	} else if (config.server.env === "development") {
+		app.listen(config.server.port, () => {
+			logger.info(`Running on ENV = '${config.server.env}'`);
+			logger.info(`Server is running at "${config.server.url}:${config.server.port}"`);
+		});
+	}
+} catch (err) {
+	//
 }
-
-app.use(bodyParser.json());
-
-await mongoInit();
-routeInit(app);
-
-app.listen(config.server.port, config.server.url, () => {
-	logger.info(`Running on ENV = '${config.server.env}'`);
-	logger.info(`Server is running at "${config.server.url}:${config.server.port}"`);
-});
