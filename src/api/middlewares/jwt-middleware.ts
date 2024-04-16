eacb356b999dca210571a3f057fedf8193193c91
Nacxht <zombiecrafter069@gmail.com>
2024-04-16 09:29:00 +0700

    fix throw error bug

--- src/api/middlewares/jwt-middleware.ts
+++ src/api/middlewares/jwt-middleware.ts
@@ -7,6 +7,7 @@ export const jwtMiddleware = async (req: Request, res: Response, next: NextFunct
 	try {
 		const authHeader = req.headers["authorization"];
 		const token = authHeader && authHeader.split(" ")[1];
+
 		if (!token) {
 			throw { name: "EmptyAuthToken" };
 		}
